d7c0c0bd275aa2efb504092df70913686f6f1e2d
Victor Kachin <victor.kachin@gmail.com>
2021-10-26 23:10:52 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const timeLeft = document.querySelector('#time-left');
+const scoreDisplay = document.querySelector('#score');
+const squares = document.querySelectorAll('.square');
+
+let molePosition
+let score = 0
+let currentTime = 60
+let countDownTimerId
+let moleTimerId
+
+function moveMole() {
+  moleTimerId = setInterval(function () {
+    squares.forEach(square => square.classList.remove('mole'))
+    const randomSquare = squares[Math.floor(Math.random() * squares.length)]
+    randomSquare.classList.add('mole')
+    molePosition = randomSquare.id
+  }, 800)
+}
+
+moveMole();
+
+squares.forEach(square => square.addEventListener('click', function () {
+  if (square.id === molePosition) {
+    score++
+    scoreDisplay.innerHTML = score
+  }
+}));
+
+function countDown() {
+  currentTime--
+  timeLeft.innerHTML = currentTime
+
+  if(currentTime === 0) {
+    clearInterval(countDownTimerId)
+    clearInterval(moleTimerId)
+  }
+};
+
+countDownTimerId = setInterval(countDown, 1000)
+
